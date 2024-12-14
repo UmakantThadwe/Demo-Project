09f5157a704986a634cf09ae56071b36b2229679
UmakantThadwe <umakant.bt@gmail.com>
2024-12-14 15:01:38 +0530

    Add files via upload

---
+++ MathOperations.java
@@ -0,0 +1,32 @@
+package javaPractice;
+
+import java.util.Scanner;
+
+public class MathOperations {
+	public static void firstNumber(int n1) {
+	System.out.print(n1);
+	return;
+	
+	}
+	public static void secondNumber(int n2) {
+	System.out.print(n2);
+	return;
+	}
+	
+	public void addition(int add) {
+		System.out.print(add);
+		return;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		
+		int add = n1+n2;
+
+	}
+
+}
